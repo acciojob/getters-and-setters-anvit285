9acce1fe0c2f6c4bb32db241f638a1d6bdd9e472
anvit285 <anvit688@gmail.com>
2025-03-27 13:35:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 
-class Person
-	{
+class Person {
   private _name: string;
   private _age: number;
-  constructor(name: string, age: number)
-	{
+
+  constructor(name: string, age: number) {
     this._name = name;
     this._age = age;
   }
-  get name(): string
-	{
-    return this._name;
-    }
 
-  get age(): number 
-	{
+  get name(): string {
+    return this._name;
+  }
+
+  get age(): number {
     return this._age;
-    }
-  set age(age: number) 
-	{
+  }
+
+  set age(age: number) {
     this._age = age;
-    }
+  }
 }
 
-class Student extends Person 
-{
-  study(): void
-	{
+class Student extends Person {
+  study(): void {
     console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach(): void {
+    console.log(`${this.name} is teaching`);
   }
 }
 
@@ -40,10 +42,11 @@ class Teacher extends Person
 }
 
 // Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+//window.Person = Person;
+//window.Student = Student;
+//window.Teacher = Teacher;
 
+export { Person, Student, Teacher };
 const person = new Person("John", 25);
 console.log(person.name); 
 
